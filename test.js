d28a68014ebdb689b0f1013f6bdaa19091176a76
rsgavrilov <rsgavrilov@mark-itt.ru>
2016-09-04 13:00:36 +0400

    fix

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var bluebird = require("bluebird");
 describe('lite-node-cache', function() {
   describe('create cacheInstance', function functionName() {
     it("create instance without arguments", function functionName() {
-      var cacheInstance = new Cache({ ttl: 1000});
+      var cacheInstance = new Cache({ ttl: 200, debugMode: false});
       assert(_.isObjectLike(cacheInstance), "cacheInstance is not a object");
       assert(_.isFunction(cacheInstance.get, "cacheInstance.get is not a function"));
       assert(_.isFunction(cacheInstance.set, "cacheInstance.set is not a function"));
@@ -21,39 +21,49 @@ describe('lite-node-cache', function() {
       assert(_.isFunction(cacheInstance.garbageCollectorAsync, "cacheInstance.garbageCollectorAsync is not a function"));
 
       describe('set new key (number), value (number), ttl is defalut and get all', function() {
-        
-        it("set keys in loop (1 to 99)", function functionName() {
-          for(let i = 0; i < 100; i++){
+
+        it("set keys in loop (1 to 10)", function functionName() {
+          for(let i = 0; i < 10; i++){
             assert(!cacheInstance.set(i, i), "set new key must return false");
             assert.strictEqual(cacheInstance.get(i, i), i);
+
+            assert(!cacheInstance.set(i+"", { prop: i}), "set new key must return false");
+            assert.strictEqual(cacheInstance.get(i+"").prop, i);
+
+            assert(!cacheInstance.set(i+"1", [i]), "set new key must return false");
+            assert.strictEqual(cacheInstance.get(i+"1")[0], i);
+
+            // assert(!cacheInstance.set({ prop: 1}, i), "set new key must return false");
+            // assert.strictEqual(cacheInstance.get({ prop: 1}, i), i);
           }
         });
 
-        it("get keys in loop (1 to 99)", function functionName() {
-          for(let i = 0; i < 100; i++){
+        it("get keys in loop (1 to 10)", function functionName() {
+          for(let i = 0; i < 10; i++){
             assert(cacheInstance.set(i, i), "if the key is rewritten should return true");
             assert.strictEqual(cacheInstance.get(i, i), i);
           }
         });
 
-        it("get keys in loop (1 to 99) after 5 second", function functionName(done) {
+        it("set keys in loop (1 to 10) after 500 ms", function functionName(done) {
           setTimeout(function functionName() {
-            for(let i = 0; i < 100; i++){
+            for(let i = 0; i < 10; i++){
               assert(cacheInstance.set(i, i), "if the key is rewritten should return true");
               assert.strictEqual(cacheInstance.get(i, i), i);
             }
             done();
-          }, 500);
+          }, 100);
         });
 
-        it("get keys in loop (1 to 99) after 5 second", function functionName(done) {
+        it("set keys in loop (1 to 10) after 1100 ms", function functionName(done) {
           setTimeout(function functionName() {
-            for(let i = 0; i < 100; i++){
-              assert(cacheInstance.set(i, i), "if the key is rewritten should return true");
+            for(let i = 0; i < 10; i++){
+              assert.strictEqual(cacheInstance.get(i, i), false);
+              assert(!cacheInstance.set(i, i), "set new key must return false");
               assert.strictEqual(cacheInstance.get(i, i), i);
             }
             done();
-          }, 500);
+          }, 300);
         });
       });
     });
